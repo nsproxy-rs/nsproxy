3e12ca678e87ce8be72522291906f4deb134577a
Guy Bortnikov <guybor97@gmail.com>
2020-04-06 01:32:13 +0300

    Remove redundant vars

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,19 @@ struct CliArgs {
 }
 
 fn parse_cli() -> CliArgs {
-    let program_name = "rproxy";
-    let program_version = "0.1.0";
-    let program_author = "Not guy";
     let target_arg_name = "target";
     let config_arg_name = "config";
     let default_config_path = env::current_dir().unwrap().as_path().join("rproxy.conf");
 
-    let matches = clap::App::new(program_name)
-    .version(program_version)
-    .author(program_author)
+    let matches = clap::App::new("rproxy")
+    .version("0.1.0")
+    .author("Guy Bortnikov, Shay Sandler")
     .arg(clap::Arg::with_name(target_arg_name)
-        .help("target program to run under rproxy")
+        .help("target program (binary) to run under rproxy")
         .required(true)
         .index(1))
     .arg(clap::Arg::with_name(config_arg_name)
-        .help("configuration path")
+        .help("configuration path, default will be taken 'rproxy.conf' from the current directory")
         .required(false)
         .index(2)
         .default_value(default_config_path.to_str().unwrap()))
