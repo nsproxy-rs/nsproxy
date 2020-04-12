eba5c4dff695e54a7260eff692fbd51746ccf0d3
sandleru <shaysandler@gmail.com>
2020-04-12 21:48:16 +0300

    config: Add structs and enums needed by nsproxy configuration

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,27 @@
-struct Config {}
+enum ProxyMethod {
+    StrictChain,
+    DynamicChain,
+}
+
+enum ProxyProtocol {
+    Socks5,
+    Socks4,
+    Http,
+}
+
+struct Auth {
+    username: std::string::String,
+    password: std::string::String,
+}
+
+struct ProxyConfig {
+    protocol: ProxyProtocol,
+    address: std::string::String,
+    port: u16,
+    auth: Auth,
+}
+
+struct Config {
+    method: ProxyMethod,
+    proxies: Vec<ProxyConfig>,
+}
